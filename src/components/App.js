72e73342b8b0a930ceaa729ee6130c0a5234423d
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2021-12-21 19:20:48 +0200

    pagetitle

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import { PageTitle } from 'components/PageTitle/PageTitle';
+
+const App = props => {
+  return (
+    <>
+      <PageTitle text="Conferencies" />
+    </>
+  );
+};
+export default App;
---
+++ src/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,7 @@
+// export const PageTitle = props => {
+//   return <h1>{props.text}</h1>;
+// };
+
+export const PageTitle = ({ text }) => {
+  return <h1>{text}</h1>;
+};
