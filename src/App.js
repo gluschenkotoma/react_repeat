1bfd016f1772ddda28240632da174827c9579170
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-01-18 15:58:48 +0200

    player

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { Reader } from 'components/Reader/Reader';
+import { Example1 } from 'components/Example1';
+import publications from './publications.json';
+const theme = {};
+
 function App() {
-  return <>Hello</>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Example1 />
+      <Reader items={publications} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
