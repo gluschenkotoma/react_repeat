3cb40c10ed730f99656407b59e5c9f65bd06bad1
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-01-25 14:42:04 +0200

    start file

--- src/components/LSReader/Reader.jsx
+++ src/components/LSReader/Reader.jsx
@@ -1,36 +1,84 @@
-import { useState, useEffect, useRef } from 'react';
-import { useMountedState } from 'react-use';
+import { Component } from 'react';
 import { Controls } from './Controls';
 import { Progress } from './Progress';
 import { Publication } from './Publication';
 
 const LS_KEY = 'reader_item_index';
 
-export const Reader = ({ items }) => {
-  const isMounted = useMountedState();
-  const [index, setIndex] = useState(() => {
-    const savedState = localStorage.getItem(LS_KEY);
-    return savedState ? Number(savedState) : 0;
-  });
-
-  const changeIndex = value => {
-    setIndex(prevIndex => prevIndex + value);
+export class Reader extends Component {
+  state = {
+    index: 0,
   };
 
-  useEffect(() => {
-    if (isMounted()) {
-      localStorage.setItem(LS_KEY, index);
+  changeIndex = value => {
+    this.setState(state => ({ index: state.index + value }));
+  };
+
+  componentDidMount() {
+    const savedState = localStorage.getItem(LS_KEY);
+    if (savedState) {
+      this.setState({ index: Number(savedState) });
     }
-  }, [index, isMounted]);
+  }
 
-  const totalItems = items.length;
-  const currentItem = items[index];
+  componentDidUpdate(_, prevState) {
+    if (prevState.index !== this.state.index) {
+      localStorage.setItem(LS_KEY, this.state.index);
+    }
+  }
 
-  return (
-    <div>
-      <Controls current={index + 1} total={totalItems} onChange={changeIndex} />
-      <Progress current={index + 1} total={totalItems} />
-      <Publication item={currentItem} />
-    </div>
-  );
-};
+  render() {
+    const { index } = this.state;
+    const { items } = this.props;
+    const totalItems = items.length;
+    const currentItem = items[index];
+
+    return (
+      <div>
+        <Controls
+          current={index + 1}
+          total={totalItems}
+          onChange={this.changeIndex}
+        />
+        <Progress current={index + 1} total={totalItems} />
+        <Publication item={currentItem} />
+      </div>
+    );
+  }
+}
+// import { useState, useEffect } from 'react';
+// import { useMountedState } from 'react-use';
+// import { Controls } from './Controls';
+// import { Progress } from './Progress';
+// import { Publication } from './Publication';
+
+// const LS_KEY = 'reader_item_index';
+
+// export const Reader = ({ items }) => {
+//   const isMounted = useMountedState();
+//   const [index, setIndex] = useState(() => {
+//     const savedState = localStorage.getItem(LS_KEY);
+//     return savedState ? Number(savedState) : 0;
+//   });
+
+//   const changeIndex = value => {
+//     setIndex(prevIndex => prevIndex + value);
+//   };
+
+//   useEffect(() => {
+//     if (isMounted()) {
+//       localStorage.setItem(LS_KEY, index);
+//     }
+//   }, [index, isMounted]);
+
+//   const totalItems = items.length;
+//   const currentItem = items[index];
+
+//   return (
+//     <div>
+//       <Controls current={index + 1} total={totalItems} onChange={changeIndex} />
+//       <Progress current={index + 1} total={totalItems} />
+//       <Publication item={currentItem} />
+//     </div>
+//   );
+// };
