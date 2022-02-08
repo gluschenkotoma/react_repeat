7eede333d63ac161cc3345ac9a81d1433da9f1a9
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-02-08 13:48:03 +0200

    start files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App';
 
-import App from './App';
+const theme = {};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
